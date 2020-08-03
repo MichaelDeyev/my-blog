c72b439dccf6885396877ba28f2309c76be9de9c
Michael Deyev <michael.deyev@gmail.com>
2020-08-03 10:27:48 +0300

    add post details page and edit/remove button

--- src/main/java/com/deyevma/myblog/controllers/BlogController.java
+++ src/main/java/com/deyevma/myblog/controllers/BlogController.java
@@ -6,6 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -22,7 +29,25 @@ public class BlogController {
 
     @GetMapping("/blog/add")
     public String blogAdd(Model model){
-
         return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title, @RequestParam String anons,
+                              @RequestParam String full_text, Model model){
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+    @GetMapping("/blog/{id}")
+    public String blogDetails(@PathVariable(value = "id") long id, Model model){
+        if (!postRepository.existsById(id)){
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blog-details";
     }
 }
